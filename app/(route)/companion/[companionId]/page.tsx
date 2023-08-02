099399749ca17ef199c74884feaadc8f729d7ea4
mayurkanade44 <mayurkanade44@gmail.com>
2023-08-02 17:35:22 +0530

    companion schema & new creation form

---
+++ app/(route)/companion/[companionId]/page.tsx
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb";
+import CompanionForm from "./components/CompanionForm";
+
+interface CompanionIdPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return (
+    <div>
+      <CompanionForm initialData={companion} categories={categories} />
+    </div>
+  );
+};
+export default CompanionIdPage;
